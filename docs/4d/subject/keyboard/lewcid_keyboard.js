1cb311ecd35e950f44de7a45e326bd4d7602d02c
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2020-06-20 15:15:21 -0700

    More tuning

--- docs/4d/subject/keyboard/lewcid_keyboard.js
+++ docs/4d/subject/keyboard/lewcid_keyboard.js
@@ -2,21 +2,21 @@
 var LEWCID_KEYBOARD = {
     "concepts":{
         "char":{count:1},
-        "col":{mod:43},
-        "line":{pack:43,mod:5},
-        "SHFT":{pack:(43*5)},
+        "col":{mod:42},
+        "line":{pack:42,mod:5},
+        "SHFT":{pack:(42*5)},
     },
     "percepts":"" +
-"`  1  2  3  4  5  6  7  8  9  0  -  =  DEL\n"+     
-"TAB q  w  e  r  t  y  u  i  o  p  [  ]  \\ \n"+     
-"CAP  a  s  d  f  g  h  j  k  l  ;  '  # R \n"+    
-"SFT   z  x  c  v  b  n  m  ,  .  /  ↑ SFT \n"+    
-"c  o  a  ____________________ c o ← ↓ →   \n"+
-"~  !  @  #  $  %  ^  &  *  (  )  _  +  del\n"+
-"tab Q  W  E  R  T  Y  U  I  O  P  {  }  | \n"+    
-"cap  A  S  D  F  G  H  J  K  L  :  \"  # ┘ \n"+     
-"sft   Z  X  C  V  B  N  M  <  >  ?  ↑ sft \n"+   
-"c  o  a  ____________________ c o ← ↓ →   \n"+
+"`  1  2  3  4  5  6  7  8  9  0  -  = DEL\n"+     
+"TAB q  w  e  r  t  y  u  i  o  p  [  ]  \\\n"+     
+"CAP  a  s  d  f  g  h  j  k  l  ;  '  # R\n"+    
+"SFT   z  x  c  v  b  n  m  ,  .  /  ↑ SFT\n"+    
+"c  o  a  ____________________ c o ← ↓ →  \n"+
+"~  !  @  #  $  %  ^  &  *  (  )  _  + del\n"+
+"tab Q  W  E  R  T  Y  U  I  O  P  {  }  |\n"+    
+"cap  A  S  D  F  G  H  J  K  L  :  \"  # ┘\n"+     
+"sft   Z  X  C  V  B  N  M  <  >  ?  ↑ sft\n"+   
+"c  o  a  ____________________ c o ← ↓ →  \n"+
 "",
 };
 
